5b303809dcc3e606a7dea5e80bb615cc632dee31
MichalP <michaupop@gmail.com>
2025-04-05 22:04:03 +0200

    changed the URL

--- cinema-frontend/lib/cinema-data.tsx
+++ cinema-frontend/lib/cinema-data.tsx
@@ -2,7 +2,7 @@ export async function fetchAllCinemaSchedules(cinemaNames:string[]){
     try {
         const responses = await Promise.all(
             cinemaNames.map(name =>
-                fetch(`superb-expression.railway.internal/api/cinemaschedule?cinema=${encodeURIComponent(name)}`)))
+                fetch(`http://superb-expression.railway.internal/api/cinemaschedule?cinema=${encodeURIComponent(name)}`)))
 
         const failedResponse = responses.find(response => !response.ok)
 
