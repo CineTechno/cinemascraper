a96f4425e430ecf4928ca379aa122e54c436cbef
MichalP <michaupop@gmail.com>
2025-04-06 14:23:31 +0200

    added scheduling config

--- cinema-frontend/lib/cinema-data.tsx
+++ cinema-frontend/lib/cinema-data.tsx
@@ -2,7 +2,7 @@ export async function fetchAllCinemaSchedules(cinemaNames:string[]){
     try {
         const responses = await Promise.all(
             cinemaNames.map(name =>
-                fetch(`https://superb-expression-production.up.railway.app/api/cinemaschedule?cinema=${encodeURIComponent(name)}`)))
+                fetch(`http://localhost:8080/api/cinemaschedule?cinema=${encodeURIComponent(name)}`)))
 
         const failedResponse = responses.find(response => !response.ok)
 
--- src/main/java/com/cinemascraper/configuration/SchedulingConfig.java
+++ src/main/java/com/cinemascraper/configuration/SchedulingConfig.java
@@ -1,5 +1,11 @@
 package com.cinemascraper.configuration;
 
+import com.cinemascraper.filmRepository.FilmRepository;
+import com.cinemascraper.service.Scrapers.ScraperService;
+import com.cinemascraper.service.tmdb.TMDBMovieService;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,18 +15,41 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.io.IOException;
+import java.util.List;
+
 @EnableScheduling
 @Configuration
 public class SchedulingConfig {
-
     @Autowired
     private JdbcTemplate jdbcTemplate;
     private static final Logger log = LoggerFactory.getLogger(SchedulingConfig.class);
+    @Autowired
+    private OkHttpClient client;
+    @Autowired
+    private ScraperService scraperService;
+    @Autowired
+    private FilmRepository filmRepository;
+    @Autowired
+    TMDBMovieService tmdbMovieService;
 
     @Scheduled(cron = "0 0 0 * * ?")
     public void deleteOldShowtimes() {
+
         String sql = "DELETE FROM showtimes WHERE show_datetime < CURRENT_DATE";
         jdbcTemplate.update(sql);
         log.info("Old showtimes deleted successfully");
+    }
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void scrapeFilms() {
+        log.info("Scraping films");
+        scraperService.scrapeAll();
+
+        List<String> titles = filmRepository.getTitlesFromDB();
+
+        try {
+            tmdbMovieService.addRatingFromTMDB(titles);
+        }   catch (Exception e) {}
     }
 }
